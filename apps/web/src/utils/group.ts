6051f2c48ee49db6c78cadbb8bb03934730ba022
Daniel Mateos <dmateosc@outlook.es>
2023-03-28 13:32:05 +0200

    feat: only astro

--- apps/web/src/utils/group.ts
+++
@@ -1,8 +0,0 @@
-export const groupList = <T extends unknown>(list: T[], size: number): T[][] => {
-  const groups = [];
-  for (let i = 0; i < list.length; i += size) {
-    groups.push(list.slice(i, i + size));
-  }
-  
-  return groups;
-}
